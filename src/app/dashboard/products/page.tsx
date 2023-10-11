4a863196e011e446c771279b23656236e213ae75
Rene Meza <renemeza.escamilla@gmail.com>
2023-10-11 15:57:07 -0700

    fix: problem with build products

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -20,107 +20,105 @@ import { UserNav } from "@/app/components/table/components/user-nav";
 import { useGetProducts } from "./useProduct";
 
 export default function Products() {
-  const { isLoading, data, isSuccess } = useGetProducts();
-
-  console.log(data?.data.products);
   return (
-    <Container>
-      <div className="md:hidden">
-        <Image
-          src="/examples/tasks-light.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/tasks-dark.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-        <div className="flex items-center justify-between space-y-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">Products</h2>
-          </div>
-        </div>
-        <DataTable
-          data={data?.data.products}
-          columns={columns}
-          isLoading={isLoading}
-        />
-      </div>
-    </Container>
+    <div></div>
     // <Container>
-    //   <div className={styles.section}>
-    //     <div className={styles.billingSection}>
-    //       <Card>
-    //         <div>
-    //           <Strong>Billing</Strong> : 1500
-    //         </div>
-    //         <div>
-    //           <Strong>Profit</Strong> : 1200
-    //         </div>
-    //       </Card>
+    //   <div className="md:hidden">
+    //     <Image
+    //       src="/examples/tasks-light.png"
+    //       width={1280}
+    //       height={998}
+    //       alt="Playground"
+    //       className="block dark:hidden"
+    //     />
+    //     <Image
+    //       src="/examples/tasks-dark.png"
+    //       width={1280}
+    //       height={998}
+    //       alt="Playground"
+    //       className="hidden dark:block"
+    //     />
+    //   </div>
+    //   <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+    //     <div className="flex items-center justify-between space-y-2">
+    //       <div>
+    //         <h2 className="text-2xl font-bold tracking-tight">Products</h2>
+    //       </div>
     //     </div>
-    //     <div>
-    //       <Button>Request Payment</Button>
-    //     </div>
-    //     <div>
-    //       <ScrollArea
-    //         type="always"
-    //         scrollbars="vertical"
-    //         style={{ height: 250 }}
-    //       >
-    //         <Box p="2" pr="8">
-    //           <Heading size="4" mb="2" trim="start">
-    //             Order List
-    //           </Heading>
-    //           <Flex direction="column" gap="4">
-    //             <Card>
-    //               <Flex gap="3" align="center">
-    //                 <Avatar
-    //                   size="3"
-    //                   src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-    //                   radius="full"
-    //                   fallback="T"
-    //                 />
-    //                 <Box>
-    //                   <Text as="div" size="2" weight="bold">
-    //                     Teodros Girmay
-    //                   </Text>
-    //                   <Text as="div" size="2" color="gray">
-    //                     Engineering
-    //                   </Text>
-    //                 </Box>
-    //               </Flex>
-    //             </Card>
-    //             <Card>
-    //               <Flex gap="3" align="center">
-    //                 <Avatar
-    //                   size="3"
-    //                   src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-    //                   radius="full"
-    //                   fallback="T"
-    //                 />
-    //                 <Box>
-    //                   <Text as="div" size="2" weight="bold">
-    //                     Teodros Girmay
-    //                   </Text>
-    //                   <Text as="div" size="2" color="gray">
-    //                     Engineering
-    //                   </Text>
-    //                 </Box>
-    //               </Flex>
-    //             </Card>
-    //           </Flex>
-    //         </Box>
-    //       </ScrollArea>
-    //     </div>
+    //     <DataTable
+    //       data={data?.data.products}
+    //       columns={columns}
+    //       isLoading={isLoading}
+    //     />
     //   </div>
     // </Container>
+    // // <Container>
+    // //   <div className={styles.section}>
+    // //     <div className={styles.billingSection}>
+    // //       <Card>
+    // //         <div>
+    // //           <Strong>Billing</Strong> : 1500
+    // //         </div>
+    // //         <div>
+    // //           <Strong>Profit</Strong> : 1200
+    // //         </div>
+    // //       </Card>
+    // //     </div>
+    // //     <div>
+    // //       <Button>Request Payment</Button>
+    // //     </div>
+    // //     <div>
+    // //       <ScrollArea
+    // //         type="always"
+    // //         scrollbars="vertical"
+    // //         style={{ height: 250 }}
+    // //       >
+    // //         <Box p="2" pr="8">
+    // //           <Heading size="4" mb="2" trim="start">
+    // //             Order List
+    // //           </Heading>
+    // //           <Flex direction="column" gap="4">
+    // //             <Card>
+    // //               <Flex gap="3" align="center">
+    // //                 <Avatar
+    // //                   size="3"
+    // //                   src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+    // //                   radius="full"
+    // //                   fallback="T"
+    // //                 />
+    // //                 <Box>
+    // //                   <Text as="div" size="2" weight="bold">
+    // //                     Teodros Girmay
+    // //                   </Text>
+    // //                   <Text as="div" size="2" color="gray">
+    // //                     Engineering
+    // //                   </Text>
+    // //                 </Box>
+    // //               </Flex>
+    // //             </Card>
+    // //             <Card>
+    // //               <Flex gap="3" align="center">
+    // //                 <Avatar
+    // //                   size="3"
+    // //                   src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+    // //                   radius="full"
+    // //                   fallback="T"
+    // //                 />
+    // //                 <Box>
+    // //                   <Text as="div" size="2" weight="bold">
+    // //                     Teodros Girmay
+    // //                   </Text>
+    // //                   <Text as="div" size="2" color="gray">
+    // //                     Engineering
+    // //                   </Text>
+    // //                 </Box>
+    // //               </Flex>
+    // //             </Card>
+    // //           </Flex>
+    // //         </Box>
+    // //       </ScrollArea>
+    // //     </div>
+    // //   </div>
+    // // </Container>
   );
 }
