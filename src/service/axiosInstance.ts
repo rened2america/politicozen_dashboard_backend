d241c9b30404a61b1d3ed6c2555e40e8254cbd85
Rene Meza <renemeza.escamilla@gmail.com>
2023-10-11 16:42:07 -0700

    fix: problem with cors

--- src/service/axiosInstance.ts
+++ src/service/axiosInstance.ts
@@ -3,21 +3,10 @@ import axios, { HeadersDefaults } from "axios";
 const axiosClient = axios.create({
   baseURL: "https://politicozen-backend.onrender.com/api/1/",
   headers: {
-    "Content-Type": "application/json",
     Accept: "application/json",
+    "Content-Type": "application/json",
   },
   withCredentials: true,
 });
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response.status === 403) {
-      window.location.href = "/emailnotconfirm";
-    }
 
-    if (error.response.status === 401) {
-      window.location.href = "/login";
-    }
-  }
-);
 export default axiosClient;
