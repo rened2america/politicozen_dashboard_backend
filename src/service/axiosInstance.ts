7521adb520eb926154c6430fd7e28867b3b2d556
Rene Meza <renemeza.escamilla@gmail.com>
2023-10-11 17:57:09 -0700

    fix: add interceptors

--- src/service/axiosInstance.ts
+++ src/service/axiosInstance.ts
@@ -8,5 +8,16 @@ const axiosClient = axios.create({
   },
   withCredentials: true,
 });
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 403) {
+      window.location.href = "/emailnotconfirm";
+    }
 
+    if (error.response.status === 401) {
+      window.location.href = "/login";
+    }
+  }
+);
 export default axiosClient;
