1e6863103fbd5b55f051311535bc0120a608db2d
Raj <raj@d2america.com>
2024-07-10 01:35:04 +0530

    Add feature to download mockups

--- src/app/components/table/components/data-table-row-actions.tsx
+++ src/app/components/table/components/data-table-row-actions.tsx
@@ -4,6 +4,7 @@ import {
   DotsHorizontalIcon,
   Pencil2Icon,
   TrashIcon,
+  DownloadIcon
 } from "@radix-ui/react-icons";
 import { Row } from "@tanstack/react-table";
 
@@ -26,9 +27,14 @@ import { labels } from "../data/data";
 import { taskSchema } from "../data/schema";
 import { useRouter } from "next/navigation";
 import { useDeleteProduct } from "@/app/dashboard2/products/useProduct";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "@/service/axiosInstance";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import BeatLoader from "react-spinners/BeatLoader";
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>;
 }
@@ -38,6 +44,34 @@ export function DataTableRowActions<TData>({
 }: DataTableRowActionsProps<TData>) {
   const router = useRouter();
   const { mutate, isSuccess } = useDeleteProduct();
+  const [loading, setLoading] = useState(false)
+
+  const downloadImages = async (productId: number) => {
+    setLoading(true)
+    const res = await axios.get(`product/allImages/${productId}`)
+
+    let imageUrls = res.data.images.images;
+
+    const zip = new JSZip();
+    const remoteZips = imageUrls.map(async (imageUrl: any, index: number) => {
+      const response = await fetch(imageUrl.url);
+      const data = await response.blob();
+      zip.file(`${index + 1}.webp`, data);
+
+      Promise.all(remoteZips)
+        .then(() => {
+          zip.generateAsync({ type: "blob" }).then((content) => {
+            // give the zip file a name
+            saveAs(content, "mockups.zip");
+          });
+          setLoading(false);
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    });
+
+  }
 
   useEffect(() => {
     if (isSuccess) {
@@ -54,7 +88,9 @@ export function DataTableRowActions<TData>({
           variant="ghost"
           className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
         >
-          <DotsHorizontalIcon className="h-4 w-4" />
+          {loading ? <BeatLoader /> :
+            <DotsHorizontalIcon className="h-4 w-4" />
+          }
           <span className="sr-only">Open menu</span>
         </Button>
       </DropdownMenuTrigger>
@@ -79,6 +115,16 @@ export function DataTableRowActions<TData>({
           Delete
           <DropdownMenuShortcut>
             <TrashIcon />
+          </DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => {
+            downloadImages(row.original.id);
+          }}
+        >
+          Download
+          <DropdownMenuShortcut>
+            <DownloadIcon />
           </DropdownMenuShortcut>
         </DropdownMenuItem>
       </DropdownMenuContent>
